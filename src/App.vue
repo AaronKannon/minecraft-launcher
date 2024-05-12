f46996dbf6526b6b7467d13ee9bac72e0df2f683
Fernanda <wellingtonbmoraes@hotmail.com>
2024-05-12 16:30:48 -0300

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Greet from "./components/Greet.vue"
+</script>
+
+<template>
+  <div class="container">
+  <button class="play-btn bebas-neue-regular">
+    PLAY
+  </button>
+  </div>  
+</template>
+
+<style scoped>
+</style>
